516ec91e969a24131343379edb926cd29290ee67
SpruceWareDev <jokere778@gmail.com>
2023-11-26 00:38:45 +0000

    Paging implementation
    Bootinfo frame allocator :thumbsup:

--- src/main.rs
+++ src/main.rs
@@ -6,29 +6,24 @@
 
 use core::panic::PanicInfo;
 use bootloader::{BootInfo, entry_point};
-use SpruceOS::memory::translate_addr;
+use SpruceOS::memory::BootInfoFrameAllocator;
 use SpruceOS::println;
 
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
     use SpruceOS::memory;
-    use x86_64::{structures::paging::Translate, VirtAddr};
+    use x86_64::VirtAddr;
 
     println!("Spruce OS Kernel 0.0.1");
 
     SpruceOS::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let mapper = unsafe { memory::init(phys_mem_offset) };
-
-    let addresses = [0xb8000, 0x201008, 0x0100_0020_1a10, boot_info.physical_memory_offset,];
-
-    for &address in &addresses {
-        let virt = VirtAddr::new(address);
-        let phys = mapper.translate_addr(virt);
-        println!("{virt:?} -> {phys:?}");
-    }
+    let mut _mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut _frame_allocator = unsafe {
+        BootInfoFrameAllocator::init(&boot_info.memory_map)
+    };
 
     #[cfg(test)]
     test_main();
--- src/memory.rs
+++ src/memory.rs
@@ -1,9 +1,14 @@
 use x86_64::{
-    structures::paging::{PageTable, OffsetPageTable},
+    structures::paging::{
+        PageTable, OffsetPageTable,
+        FrameAllocator, Size4KiB,
+        PhysFrame
+    },
     VirtAddr,
     PhysAddr,
 };
 use x86_64::registers::control::Cr3;
+use bootloader::bootinfo::{MemoryMap, MemoryRegionType};
 
 pub unsafe fn init(phys_mem_offset: VirtAddr) -> OffsetPageTable<'static> {
     let level_4_table = active_level_4_table(phys_mem_offset);
@@ -27,4 +32,42 @@ unsafe fn active_level_4_table(phys_mem_offset: VirtAddr) -> &'static mut PageTa
 
     // unsafe
     &mut *page_table_ptr
+}
+
+/// --------------------------------------
+/// Boot Info Allocator
+/// --------------------------------------
+pub struct BootInfoFrameAllocator {
+    memory_map: &'static MemoryMap,
+    next: usize,
+}
+
+impl BootInfoFrameAllocator {
+    pub unsafe fn init(memory_map: &'static MemoryMap) -> Self {
+        BootInfoFrameAllocator {
+            memory_map,
+            next: 0,
+        }
+    }
+
+    /// Returns an iterator of the usable frames specified in the memory map.
+    fn usable_frames(&self) -> impl Iterator<Item = PhysFrame> {
+        // Get usable regions from memory map
+        let regions = self.memory_map.iter();
+        let usable_regions = regions.filter(|r| r.region_type == MemoryRegionType::Usable);
+        // Map each region to its address range
+        let addr_ranges = usable_regions.map(|r| r.range.start_addr()..r.range.end_addr());
+        // Transform to an iterator of frame start addresses
+        let frame_addresses = addr_ranges.flat_map(|r| r.step_by(4096));
+        // Create `PhyFrame` types from the start addresses
+        frame_addresses.map(|addr| PhysFrame::containing_address(PhysAddr::new(addr)))
+    }
+}
+
+unsafe impl FrameAllocator<Size4KiB> for BootInfoFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame<Size4KiB>> {
+        let frame = self.usable_frames().nth(self.next);
+        self.next += 1;
+        frame
+    }
 }
